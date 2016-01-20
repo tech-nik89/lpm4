21116e7dd92bb36d92e73ad961d1f57ac9124fa3
tech-nik89 <gh@t-hd.net>
2016-01-20 21:29:17 +0100

    License adjustments

--- index.php
+++ index.php
@@ -3,27 +3,11 @@
 	/**
 	 * Project: Higher For Hire
 	 * File: index.php
-	 * Author: Nikolaus Themessl
-	 * 
-	 * Copyright (C) 2009-2012 Nikolaus Themessl
-	 *
 	 *
      * This file is part of LAN Party Manager 4.
 	 *
-	 * LAN Party Manager 4 is free software: you can redistribute it and/or modify
-	 * it under the terms of the GNU Lesser General Public License as published by
-	 * the Free Software Foundation, either version 3 of the License, or
-	 * (at your option) any later version.
-	 *
-	 * LAN Party Manager 4 is distributed in the hope that it will be useful,
-	 * but WITHOUT ANY WARRANTY; without even the implied warranty of
-	 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	 * GNU Lesser General Public License for more details.
-	 *
-	 * You should have received a copy of the GNU Lesser General Public License
-	 * along with LAN Party Manager 4.  If not, see <http://www.gnu.org/licenses/>.
-	 *
-	 *
+	 * LAN Party Manager 4 is licensed under the MIT License. See the 
+	 * "LICENSE" file for more details.
 	 *
 	 *
 	 * LAN Party Manager 4 (Higher For Hire) is using Smarty Template Engine
